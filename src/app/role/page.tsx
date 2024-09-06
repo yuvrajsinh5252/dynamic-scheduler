12a8aa6e89f5dc0f91f3e8a958abc77531d8d5c0
Yuvrajsinh Gohil <yuvrajsinh476@gmail.com>
2024-09-07 00:26:47 +0530

    role page set to client

--- src/app/role/page.tsx
+++ src/app/role/page.tsx
@@ -1,41 +1,55 @@
+"use client"
+
 import { User } from "lucide-react";
 import { getUserRole, setUserRole } from "../actions";
-import { redirect } from 'next/navigation'
-import { auth } from "@/lib/auth";
+import { useRouter } from 'next/navigation'
+import { useEffect } from "react";
+import { Role } from "@prisma/client";
+import { useSession } from "next-auth/react";
 
-export default async function Page() {
-  const session = await auth();
+export default function Page() {
+  const session = useSession();
+  const router = useRouter()
 
-  const role = await getUserRole(session?.user?.id ?? "");
-  if (role != undefined) {
-    redirect("/dashboard");
-  }
+  useEffect(() => {
+    const fetchData = async () => {
+      if (session.data?.user?.id) {
+        const role = await getUserRole(session.data?.user?.id);
+        if (role) {
+          router.push("/dashboard");
+        }
+      }
+    };
+
+    fetchData();
+  }, [session.data?.user?.id]);
+
+  const handleUserRole = async (newRole: Role) => {
+    if (session.data?.user?.id) {
+      await setUserRole(session.data?.user?.id, newRole);
+      router.push("/dashboard");
+    }
+  };
 
   return (
     <div className="h-screen flex justify-center items-center">
       <div>
         <h1>Choose a role to proceed</h1>
-        <div className="flex justify-around gap-4 p-2">
+        <div className="flex justify-around gap-4 p-2 cursor-pointer">
           <div
-            onClick={async () => {
-              await setUserRole(session?.user?.id ?? "", "USER");
-              redirect("/dashboard");
-            }}
+            onClick={() => handleUserRole("USER")}
             className="flex flex-col justify-center items-center">
             <User size={40} />
             <h2>User</h2>
           </div>
           <div
-            onClick={async () => {
-              await setUserRole(session?.user?.id ?? "", "ADMIN");
-              redirect("/dashboard");
-            }}
+            onClick={() => handleUserRole("ADMIN")}
             className="flex flex-col justify-center items-center">
             <User size={40} />
             <h2>Admin</h2>
           </div>
         </div>
       </div>
-    </div >
+    </div>
   );
 }
